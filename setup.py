024fe80bbc2629b44d9de87c0fa7d016765ab06c
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-02-05 00:13:58 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup
+
+
+
+
+
+setup(
+    name="PyQt5-Encryption",
+    author="Ashutosh",
+    maintainer="Kyle Altendorf, Bryce Beagle, Florian Bruhin",
+    maintainer_email="sda@fstab.net",
+    description=" PyQt5 framework",
+
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development"
+    ]
+)
